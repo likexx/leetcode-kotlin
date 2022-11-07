a4015ed4b4543561724275ebb21f019148e06158
likezhang <likexx@live.com>
2022-11-07 18:14:18 +0800

    solution 952 & 1323

---
+++ app/src/test/kotlin/test/Test1323.kt
@@ -0,0 +1,29 @@
+package test
+
+import org.junit.Before
+import kotlin.test.Test
+import kotlin.test.*
+
+class Test1323 : TestBase<Solution1323.Solution>(Solution1323.Solution()) {
+
+    @Test
+    fun test1() {
+        val s = Solution1323.Solution()
+        assertEquals(9999, s.maximum69Number(9996))
+        assertEquals(9999, s.maximum69Number(9999))
+        assertEquals(99999, s.maximum69Number(99969))
+        assertEquals(99969, s.maximum69Number(96969))
+        assertEquals(9969, s.maximum69Number(9669))
+    }
+
+    @Test
+    fun test2() {
+        val s = Solution1323.Solution()
+        assertEquals(9999, s.maximum69NumberOriginal(9996))
+        assertEquals(9999, s.maximum69NumberOriginal(9999))
+        assertEquals(99999, s.maximum69NumberOriginal(99969))
+        assertEquals(99969, s.maximum69NumberOriginal(96969))
+        assertEquals(9969, s.maximum69NumberOriginal(9669))
+    }
+
+}
---
+++ app/src/test/kotlin/test/Test952.kt
@@ -0,0 +1,17 @@
+package test
+
+import org.junit.Before
+import kotlin.test.Test
+import kotlin.test.*
+
+class Test952 : TestBase<Solution952.Solution>(Solution952.Solution()) {
+
+    @Test
+    fun test1() {
+        val s = Solution952.Solution()
+        assertEquals(4, s.largestComponentSize(intArrayOf(4,6,15,35)))
+//        assertEquals(2, s.largestComponentSize(intArrayOf(20,50,9,63)))
+//        assertEquals(8, s.largestComponentSize(intArrayOf(2,3,6,7,4,12,21,39)))
+    }
+
+}
