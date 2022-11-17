128878590557a46645c10a9c254c7671750a848e
likezhang <likexx@live.com>
2022-11-17 17:44:39 +0800

    add empty unit test

--- app/src/test/kotlin/test/Test38.kt
+++ app/src/test/kotlin/test/Test38.kt
@@ -1,4 +1,11 @@
 package test
 
-class Test38 {
+import org.junit.Test
+import solution.Solution38
+
+class Test38 : TestBase<Solution38.Solution>(Solution38.Solution()) {
+    @Test
+    fun test() {
+
+    }
 }
---
+++ app/src/test/kotlin/test/Test49.kt
@@ -0,0 +1,11 @@
+package test
+
+import org.junit.Test
+import solution.Solution49
+
+class Test49 : TestBase<Solution49.Solution>(Solution49.Solution()) {
+    @Test
+    fun test() {
+
+    }
+}
